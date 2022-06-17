bbfa93fbd04f9557df418577b05bf42f61596947
goodgksk4886 <84364731+goodgksk4886@users.noreply.github.com>
2022-06-17 09:17:39 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
 basic.forever(function () {
-    basic.showIcon(IconNames.Surprised)
-    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+    basic.showIcon(IconNames.Sword)
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 60)
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.GREEN)
     } else {
         if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 160)
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 30)
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 60)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.BLUE)
         }
         if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 60)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.YELLOW)
+        }
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
             DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 30)
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 160)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 60)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.YELLOW)
         }
     }
 })
